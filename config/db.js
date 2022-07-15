9d709df7f6392aae6e8de65faf1003fbf2f39a73
Italo Pinheiro <italo_00pinheiro@hotmail.com>
2022-07-15 15:13:55 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@
 // código para diferenciar o link de produção ou de localhost
 
 if(process.env.NODE_ENV == "production"){
-    module.exports = {mongoURI: "mongodb+srv://italopinheiro:pinheiro97@blog-app.jb5ys.mongodb.net/portal-do-aluno"}
+    module.exports = {mongoURI: ""}
 }else{
     module.exports = {mongoURI: "mongodb://localhost/portal-do-aluno"}
 }
